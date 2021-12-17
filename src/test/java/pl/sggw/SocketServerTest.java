c431bf7f8c31c851fe8cb74747641f34fbf7b9d6
Jakub Płuciennik <pluciennik.jakub@gmail.com>
2021-12-17 22:18:21 +0100

    Testy do SocketServer napisane w Selenide: testManage: otworzenie strony manage.html i usunięcie wszystkich książek testBooksEmpty: usunięcie książek i sprawdzenie, czy zostały usunięte testAddBook: dodanie jedniej książki i sprawdzenie czy zawartość się zgadza testAddBooks2: dodanie 2 książek i sprawdzenie czy zostały one dodanie testUpdateBook: dodanie książki, edycja jej danych i sprawdzenie, czy dane książki równe są edycji

---
+++ src/test/java/pl/sggw/SocketServerTest.java
@@ -0,0 +1,125 @@
+package pl.sggw;
+
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.refresh;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+class SocketServerTest {
+  static Properties prop;
+  static String path = "";
+
+  static {
+    prop = new Properties();
+    Configuration.timeout = 25 * 1000;
+    try {
+      prop.load(new FileInputStream("src/main/resources/application.properties"));
+    } catch (IOException e) {
+      e.printStackTrace();
+      try {
+        prop.load(new FileInputStream("src/test/resources/application.properties"));
+      } catch (IOException f) {
+        f.printStackTrace();
+        prop.put("port", "8080");
+        prop.put("host", "http://localhost");
+      }
+    }
+    path = prop.get("host") + ":" + prop.get("port");
+  }
+
+  @BeforeEach
+  synchronized void clearBooks() {
+    open(path + "/manage.html");
+    $("form").$("input[type=submit]").click();
+  } //czyszczenie między testami
+
+  @Test
+  synchronized void testManage() {  //sprawdzenie czy na stronie manage jest formularz z /clearBooksAction
+    open(path + "/manage.html");
+    Assertions.assertEquals($("form").getDomAttribute("action"), "/clearBooksAction");
+    $("form").$("input[type=submit]").click();  //czyszczenie książek
+  }
+
+  @Test
+  synchronized void testBooksEmpty() {  //sprawdzanie czy po wyczyszczeniu biblioteki jest tekst "Brak książek"
+    clearBooks();
+    open(path + "/books.html");
+    Assertions.assertTrue($("div").getOwnText().equalsIgnoreCase("Brak książek"));
+  }
+
+
+  @Test
+  synchronized void testAddBook() {
+    clearBooks();
+    open(path + "/addBook.html");
+    Assertions.assertTrue($("form").getDomAttribute("method").equalsIgnoreCase("post"));  //sprawdzanie czy addBooks jest POSTem
+    String title = "Tytuł 1";
+    String authorName = "Imię 1";
+    String authorSurname = "Nazwisko 1";
+    $("form").$("input[name=\"title\"]").setValue(title);
+    $("form").$("input[name=\"authorName\"]").setValue(authorName);
+    $("form").$("input[name=\"authorSurname\"]").setValue(authorSurname);
+    $("form").$("input[type=submit]").click();
+
+    //sprawdzenie czy w tabeli pierwsza linia jest równa wysłym wartościom
+    $$("tbody tr").shouldHave(size(1));
+    $("tbody tr").$(".id").shouldBe(Condition.text("1"));
+    $("tbody tr").$(".title").shouldBe(Condition.text(title));
+    $("tbody tr").$(".authorName").shouldBe(Condition.text(authorName));
+    $("tbody tr").$(".authorSurname").shouldBe(Condition.text(authorSurname));
+  }
+
+  @Test
+  void testAddBooks2() {  //dodanie 5 książek pod rząd
+    clearBooks();
+    for (int i = 1; i <= 2; i++) {
+      open(path + "/addBook.html");
+      $("table").should(Condition.disappear);
+      $("form").shouldBe(Condition.visible);
+      String title = "Tytuł " + i;
+      String authorName = "Imię "+i;
+      String authorSurname = "Nazwisko "+i;
+      $("form").$("input[name=\"title\"]").setValue(title);
+      $("form").$("input[name=\"authorName\"]").setValue(authorName);
+      $("form").$("input[name=\"authorSurname\"]").setValue(authorSurname);
+      $("form").$("input[type=submit]").click();
+    }
+    open(path+"/books.html");
+    refresh();
+
+    $$("tbody tr").shouldBe(size(2));
+  }
+
+  @Test
+  synchronized void testUpdateBook() {
+    testAddBook();  //dodanie książki z indeksem 1
+
+    open(path + "/updateBook.html?id=1");
+    Assertions.assertTrue(
+        $("form").getDomAttribute("action").equalsIgnoreCase("/updateBookAction"));  //istnieje formularz do zmiany książki z indeksem 1
+
+    $("form input[name=\"title\"]").setValue("Edycja");
+    $("form input[name=\"authorName\"]").setValue("Edycja");
+    $("form input[name=\"authorSurname\"]").setValue("Edycja");
+
+    $("form input[type=submit]").click();
+
+    open(path + "/books.html");
+
+    $("tbody tr .title").shouldBe(Condition.text("Edycja"));
+    $("tbody tr .authorName").shouldBe(Condition.text("Edycja"));
+    $("tbody tr .authorSurname").shouldBe(Condition.text("Edycja"));
+  }
+}
+//Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
